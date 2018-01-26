3bd1d3b1e37546f30d7763ecd2d3400a5a0a872b
Hoakle <n.wanhem@gmail.com>
2018-01-26 23:06:46 +0100

    Modification du path de projet

---
+++ Assets/Scripts/Shape.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TeamEnum { Neutral, Red, Bleu, }
+
+public class Shape : MonoBehaviour {
+    public TeamEnum team = TeamEnum.Neutral;
+
+    void OnCollisionEnter (Collision other)
+    {
+        Shape otherShape = other.gameObject.GetComponent<Shape>();
+        if (other.gameObject.CompareTag("Shape"))
+        {
+            if (team != TeamEnum.Neutral) {
+                if (otherShape.team == TeamEnum.Neutral)
+                {
+                    Debug.Log("Collision " + this.gameObject.name);
+                    CollisionNeutral(other);
+                } else if (otherShape.team != team)
+                {
+                    otherShape.DestroyGameObject(this.gameObject);
+                }
+            }
+        }
+    }
+
+    void CollisionNeutral(Collision other)
+    {
+        other.gameObject.GetComponent<Shape>().team = team;
+        other.gameObject.transform.parent = this.transform;
+    }
+
+
+    void DestroyGameObject(GameObject otherObject)
+    {
+        Destroy(otherObject);
+        Destroy(this);
+    }
+}
