a9813b8303ea2ad291567799aba9382d561ae528
lmoge <ludovic.moge@gmail.com>
2018-01-26 22:25:33 +0100

    Avatar + avatar parameters + physics material

---
+++ shapearmor/Assets/Scripts/Avatar.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Avatar : Shape
+{
+    public AvatarParameters param;
+
+    private Rigidbody rigid;
+
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (input.x != 0.0f)
+        {
+            Rotate(input.x);
+        }
+
+        if (input.y != 0.0f)
+        {
+            Thrust(input.y);
+        }
+    }
+
+    private void Rotate(float input)
+    {
+        Quaternion rotation = transform.rotation;
+        Vector3 euler = rotation.eulerAngles;
+        euler.y += param.rotationSpeed * input * Time.deltaTime;
+        transform.rotation = Quaternion.Euler(euler);
+    }
+
+    private void Thrust(float input)
+    {
+        if (input > 0.0f)
+        {
+            rigid.AddForce(transform.forward * param.thrustPower * Time.deltaTime);
+        }
+    }
+
+}
---
+++ shapearmor/Assets/Scripts/AvatarParameters.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Avatar Parameters", fileName =  "New Avatar Parameters Instance", order = 0)]
+[System.Serializable]
+public class AvatarParameters : ScriptableObject
+{
+	public float thrustPower = 5.0f;
+	public float brakeForce = 15.0f;
+	public float rotationSpeed = 5.0f;
+
+}
+
+
