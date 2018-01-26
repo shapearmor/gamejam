66758ba2f39dc0e1f1c49bee1cc5db73cb96d29f
Hoakle <n.wanhem@gmail.com>
2018-01-26 21:18:01 +0100

    no message

---
+++ shapearmor/Assets/Scripts/Shape.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TeamEnum { Neutral, Red, Bleu, }
+
+public class Shape : MonoBehaviour {
+
+    [HideInInspector]
+    public TeamEnum team;
+
+    void Start()
+    {
+        team = TeamEnum.Neutral;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Shape"))
+        {
+            if (team != TeamEnum.Neutral) {
+                if(other)
+            }
+        }
+    }
+}
